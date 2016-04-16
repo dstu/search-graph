5c1d11d3b56cdca5b61b5b9f8098db7829d35193
Stu Black <trurl@freeshell.org>
2016-04-16 00:42:31 -0400

    Update rustdocs for master.

---
+++ rustdoc/master/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['search_graph'] = ["impl&lt;T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a>, R: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='enum' href='search_graph/enum.Target.html' title='search_graph::Target'>Target</a>&lt;T, R&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
